da2b4ebf42d9b01b6c6a2afdf5d4ee3b6444125e
ColdShadow80 <46814978+ColdShadow80@users.noreply.github.com>
2021-06-27 15:40:47 +0100

    Update alf.py

--- alf.py
+++ alf.py
@@ -10,11 +10,12 @@ config.read('config.ini')
 TOKEN = config['GENERAL']['TOKEN']
 ADMIN = config['GENERAL']['ADMIN']
 PREFIX = config['GENERAL']['PREFIX']
+SCRIPTS_FOLDER = config['GENERAL']['SCRIPTS_FOLDER']
 
 bot = commands.Bot(command_prefix=PREFIX, case_insensitive=1)
 
 def isadmin(ctx):
-    if ctx.author.id in [ADMIN]:
+    if ctx.author.id in [ADMIN]:  # THIS PART DOES NOT WORK AND USER ID HAS TO BE INSteAD OF ADMIN VARIABLE FOR NOW
         return True
     else:
         print(f'{ctx.author.id} is not', ADMIN, type({ctx.author.id}), type(ADMIN) )
@@ -28,6 +29,12 @@ async def restart(ctx, process):
     os.system(f"pm2 restart {process}")
     await ctx.send(f"Restarted {process}")
 
+@bot.command()
+async def script(ctx, process):
+    if not isadmin(ctx):
+        return
+    os.system(f"{SCRIPTS_FOLDER}{process}.sh")
+    await ctx.send(f"Runing {process}.sh")
 
 @bot.event
 async def on_ready():
